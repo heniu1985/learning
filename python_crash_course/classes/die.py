0dd3511992278418e4bd3790f66f02925c96def7
heniu1985 <heniu_1@o2.pl>
2020-05-24 22:47:20 +0200

    "Python Crash Course" - classes excercises Die - update

--- python_crash_course/classes/die.py
+++ python_crash_course/classes/die.py
@@ -4,9 +4,11 @@ class Die():
     """Klasa kostki do gry"""
 
     def __init__(self, sides=6):
+        """Inicjuje kostkę o podanek liczbie ścianek"""
         self.sides = sides
 
     def roll_die(self):
+        """\"Rzuca\" kostką"""
         print(randint(1, self.sides))
 
 die = Die(20)
@@ -16,3 +18,4 @@ i = 0
 while i <= 10:
     die.roll_die()
     i += 1
+    
