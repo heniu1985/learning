847cd4bd4c3d02d3eb759c99aa28853cd79f80d0
heniu1985 <heniu_1@o2.pl>
2020-05-24 22:30:01 +0200

    "Python Crash Course" - classes excercises

---
+++ python_crash_course/classes/import.py
@@ -0,0 +1,8 @@
+from restaurant import Restaurant, IceCreamStand
+
+restaurant1 = Restaurant("Szarotka", "Kuchnia regionalna")
+restaurant2 = IceCreamStand("Lodziarnia", "Budka z lodami")
+
+restaurant1.describe_restaurant()
+print("\n")
+restaurant2.show_flavors()
