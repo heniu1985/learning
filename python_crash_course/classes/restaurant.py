8216f8cb8df04dc09a3e8be971355d584d4b0e50
heniu1985 <heniu_1@o2.pl>
2020-05-24 21:22:05 +0200

    "Python Crash Course" - classes excercises

--- python_crash_course/classes/restaurant.py
+++ python_crash_course/classes/restaurant.py
@@ -5,11 +5,14 @@ class Restaurant():
         """Inicjalizacja prezentacji restauracji"""
         self.name = restaurant_name
         self.type = cusisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
+        """Wyświetla rodzaj i nazwę restauracji"""
         print(f"{self.type.title()}: {self.name.title()}")
 
     def open_restaurant(self):
+        """Wyświetla godziny otwarcia restauracji"""
         print("Poniedziałek: 12 - 20")
         print("Wtorek: 12 - 20")
         print("Środa: 12 - 20")
@@ -18,10 +21,33 @@ class Restaurant():
         print("Sobota: 12 - 2")
         print("Niedziela: nieczynne")
 
+    def set_number_served(self, served):
+        """Umożliwia zmianę liczby obsłużonych gości"""
+        self.number_served = served
+
+    def increment_number_served(self, add_served):
+        """Zwiększa liczbę obsłużonych klientów o podaną wartość"""
+        self.number_served += add_served
+
+    def show_number_served(self):
+        """Wyświetla liczbę obsłużonych klientów"""
+        print(f'W restauracji "{self.name.title()}" zostało obsłużonych {self.number_served} klientów.')
+
 la_cantina = Restaurant("la cantina", "kuchnia śródziemnomorska")
 batman = Restaurant("u batmana", "pizzeria")
 pod_lipami = Restaurant("pod lipami", "kuchnia polska")
 
-la_cantina.describe_restaurant()
-batman.describe_restaurant()
-pod_lipami.describe_restaurant()
+# la_cantina.describe_restaurant()
+# batman.describe_restaurant()
+# pod_lipami.describe_restaurant()
+
+# batman.set_number_served(23)
+# batman.show_number_served()
+
+batman.show_number_served()
+batman.increment_number_served(2)
+batman.show_number_served()
+batman.increment_number_served(4)
+batman.increment_number_served(5)
+batman.increment_number_served(9)
+batman.show_number_served()
--- python_crash_course/classes/user.py
+++ python_crash_course/classes/user.py
@@ -2,29 +2,53 @@ class User():
     """Klasa opisująca użytkownika"""
 
     def __init__(self, first_name, last_name, email, country, city):
+        """Inicjalizacja atrybutów opisujących użytkownika"""
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.country = country
         self.city = city
+        self.login_attempts = 0
 
     def describe_user(self):
+        """Wyświetla opis użytkownika"""
         print(f"{self.first_name.title()} {self.last_name.title()}")
         print(f"e-mail: {self.email}")
         print(f"{self.city.title()}, {self.country.title()}")
 
     def greet_user(self):
+        """Spersonalizowane powitanie"""
         print(f"Witaj {self.first_name.title()}")
+
+    def increment_login_attempts(self):
+        """Zwiększa liczbę prób logowania"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resetuje próby logowania"""
+        self.login_attempts = 0
+
+    def show_login_attempts(self):
+        """Wyświetla liczbę prób logowania"""
+        print(f"{self.login_attempts} prób logowania.")
 
 user1 = User("Piotr", "Kowalski", "pkowalski@gmail.com", "Polska", "Rzeszów")
 user2 = User("Damian", "Hennek", "hennek@gmail.com", "Polska", "Katowice")
-user3 = User("John", "Kowalsky", "john.kowalsky@yahoo.com", "usa", "new york")
+user3 = User("John", "Kowalsky", "john.kowalsky@yahoo.com", "usa", "new")
 
-user1.greet_user()
-user1.describe_user()
+# user1.greet_user()
+# user1.describe_user()
 
-user2.greet_user()
-user2.describe_user()
+# user2.greet_user()
+# user2.describe_user()
 
-user3.greet_user()
-user3.describe_user()
+# user3.greet_user()
+# user3.describe_user()
+
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.show_login_attempts()
+user1.reset_login_attempts()
+user1.show_login_attempts()
