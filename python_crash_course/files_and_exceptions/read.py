d806b2baf79d80f84d648f06f61f2e25df79f115
heniu1985 <heniu_1@o2.pl>
2020-05-25 18:29:42 +0200

    "Python Crash Course" Files and Exceptions

---
+++ python_crash_course/files_and_exceptions/read.py
@@ -0,0 +1,34 @@
+file_path = "/home/heniu/Programowanie/Nauka/Python/learning/python_crash_course/files_and_exceptions/learning_python.txt"
+
+# Odczyt całego pliku
+print("\nPrzykład pierwszy")
+
+with open(file_path) as file_object:
+    content = file_object.read()
+print(content)
+
+# Odczyt przez iterację pętlą for
+print("\nPrzykład drugi")
+
+with open(file_path) as file_object:
+    for line in file_object:
+        print(line.strip())
+
+# Odczyt pliku przez umieszczenie wierszy na liście
+print("\nPrzykład trzeci")
+
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.strip())
+
+# Zmiana słowa
+print("\nZmiana słowa Python na C")
+
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    line = line.replace("Pythonie", "C")
+    print(line.rstrip())
