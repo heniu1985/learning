8bbfceeda59f2bb4edfb95d70de5bd6414a44f67
heniu1985 <heniu_1@o2.pl>
2020-05-28 19:22:28 +0200

    "Python Crash Course" - Project 2 15.1 Cubes

---
+++ python_crash_course/project2/cubes_chart.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+FILE_PATH = "python_crash_course/project2/cubes_plot.png"
+
+x_values = range(1, 5001)
+y_values = [x ** 3 for x in x_values]
+
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values, c="Green", linewidth=1)
+
+ax.set_title("Sześciany liczb", fontsize=24)
+ax.set_xlabel("Wartość", fontsize=12)
+ax.set_ylabel("Sześciany wartości", fontsize=12)
+
+ax.tick_params(axis="both", labelsize=8)
+
+plt.ticklabel_format(useMathText=True)
+
+plt.show()
