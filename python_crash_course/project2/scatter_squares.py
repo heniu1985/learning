66eaa440adfa05c4dd19cc5d4544845ff0b644d0
heniu1985 <heniu_1@o2.pl>
2020-05-28 18:21:45 +0200

    "Python Crash Course" - Project 2

--- python_crash_course/project2/scatter_squares.py
+++ python_crash_course/project2/scatter_squares.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+FILE_PATH = "python_crash_course/project2/squares_plot.png"
+
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
@@ -20,4 +22,8 @@ ax.axis([0, 1100, 0, 1100000])
 
 plt.ticklabel_format(useMathText=True)
 
+# Pokazuje wykres
 plt.show()
+
+# Zapisuje wykres do pliku
+plt.savefig(FILE_PATH, bbox_inches="tight")
