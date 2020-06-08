3a1121208f14f5dfadc2c9bbcf443ee6c142921b
heniu1985 <heniu_1@o2.pl>
2020-06-08 17:37:24 +0200

    "Python Crash Course" - Project 2

--- python_crash_course/project2/die_visual.py
+++ python_crash_course/project2/die_visual.py
@@ -3,27 +3,29 @@ from plotly import offline
 
 from die import Die
 
-FILE_PATH = "python_crash_course/project2/d6.html"
+FILE_PATH = "python_crash_course/project2/2xd6.html"
 # Utworzenie kości do gry
-die = Die()
+die1 = Die()
+die2 = Die()
 
 # Wykonanie pewnej liczby rzutów i umieszczenie wyników na liście.
 results = []
 for roll_num in range(1_000):
-    result = die.roll()
+    result = die1.roll() + die2.roll()
     results.append(result)
 
 # Analiza wyników.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Wizualizacja wyników.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {"title": "Wynik"}
 y_axis_config = {"title": "Częstotliwość występowania wartorści"}
-my_layout = Layout(title="Wynik rzucania pojedynczą kością D6 tysiąc razy", xaxis=x_axis_config, yaxis=y_axis_config)
+my_layout = Layout(title="Wynik rzucania dwoma kościami D6 tysiąc razy", xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({"data": data, "layout": my_layout}, filename=FILE_PATH)
