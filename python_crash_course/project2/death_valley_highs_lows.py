beee9295b71e4a83addb6b842ffee669dd8935dc
heniu1985 <heniu_1@o2.pl>
2020-06-08 19:02:13 +0200

    "Python Crash Course" - Project 2

--- python_crash_course/project2/death_valley_highs_lows.py
+++ python_crash_course/project2/death_valley_highs_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-FILE_PATH = "python_crash_course/project2/data/sitka_weather_2018_simple.csv"
+FILE_PATH = "python_crash_course/project2/data/death_valley_2018_simple.csv"
 
 filename = FILE_PATH
 with open(filename) as f:
@@ -14,11 +14,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Brak danych dla {current_date}.")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Dane wykresu.
 plt.style.use("seaborn")
@@ -28,7 +32,7 @@ ax.plot(dates, lows, c="blue", alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Formatowanie wykresu
-ax.set_title("Najwyższa i najniższa temeratura dnia - 2018", fontsize=24)
+ax.set_title("Najwyższa i najniższa temeratura dnia - 2018\nDolina Śmierci", fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperatura (F)", fontsize=16)
